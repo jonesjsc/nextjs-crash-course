5b0559183b7ea38e23b3430c1753dd43544c5715
Jason Jones <jason.e.jones@gmail.com>
2021-08-15 21:45:16 -0400

    Head - and the exlintrc and babelrc files needed to keep lint errors from happening

--- pages/about.js
+++ pages/about.js
@@ -1,7 +1,12 @@
+import Head from 'next/head';
+
 const about = () => {
   return (
     <div>
-      <h1>About</h1>
+      <Head>
+        <title>ABOUT</title>
+        <meta name='keywords' content='web development, programming' />
+      </Head>
     </div>
   );
 };
