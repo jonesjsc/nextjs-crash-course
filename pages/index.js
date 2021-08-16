b46092f9b0bd9280980bf2f0973147e9e7ce40bf
Jason Jones <jason.e.jones@gmail.com>
2021-08-15 22:54:10 -0400

    Showing Data

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,29 @@
 import Head from 'next/head';
+import ArticleList from '../components/ArticleList';
 
-export default function Home() {
+export default function Home({ articles }) {
   return (
     <div>
       <Head>
         <title>My Title</title>
         <meta name='keywords' content='web development, programming' />
       </Head>
-      <h1>Welcome to Next</h1>
+
+      <ArticleList articles={articles} />
     </div>
   );
 }
+
+export const getStaticProps = async () => {
+  // fetch 6 posts from jsonplaceholder.typicode.com
+  const res = await fetch(
+    'https://jsonplaceholder.typicode.com/posts?_limit=6'
+  );
+
+  const articles = await res.json();
+  return {
+    props: {
+      articles,
+    },
+  };
+};
